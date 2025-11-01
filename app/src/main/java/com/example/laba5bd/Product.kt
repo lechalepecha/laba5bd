507f43e6ddb466c572dcf18652bbfe7b31b95aa8
lechalepecha <aleksejserakov45@gmail.com>
2025-11-01 16:52:27 +0700

    product

---
+++ app/src/main/java/com/example/laba5bd/Product.kt
@@ -0,0 +1,12 @@
+package com.example.laba5bd
+
+class Product {
+    var id: Int = 0
+    var productName: String = ""
+    var quantity: Int = 0
+    constructor()
+    constructor(productname: String, quantity: Int) {
+        this.productName = productname
+        this.quantity = quantity
+    }
+}
