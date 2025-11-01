e9a0904845a51ff4d411f9b18ce21fbd9ee83389
lechalepecha <aleksejserakov45@gmail.com>
2025-11-01 16:53:38 +0700

    product now entity

--- app/src/main/java/com/example/laba5bd/Product.kt
+++ app/src/main/java/com/example/laba5bd/Product.kt
@@ -1,7 +1,14 @@
 package com.example.laba5bd
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
 class Product {
-    var id: Int = 0
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "productId")var id: Int = 0
+    @ColumnInfo(name = "productName")
     var productName: String = ""
     var quantity: Int = 0
     constructor()
